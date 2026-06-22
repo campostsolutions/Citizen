0e7ff26e098190832100aa8de533711384d698cc
Andy Millard <support@campostsolutions.co.uk>
2026-06-22 10:16:31 +0100

    Remove tests folder from git tracking and add to gitignore

--- tests/recreate-expected-all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem The first argument is optional post engine path.
-
-set EXTRA_FLAGS=--fit --problemsOnly
-
-call %~dp0\run-all.bat %1
--- tests/recreate-expected-single.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem The first argument is the post file name
-
-set EXTRA_FLAGS=--fit --problemsOnly
-
-call %~dp0\run-single.bat %1
